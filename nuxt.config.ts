0825cc83ded9ccb23245c7ad81ab9b4d313ab990
rubatista <inforubenbaptista@gmail.com>
2025-04-15 18:32:21 +0100

    feat: installed configurations and started to create pages

---
+++ nuxt.config.ts
@@ -0,0 +1,36 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-04-15',
+  future: {
+    compatibilityVersion: 4,
+  },
+  experimental: {
+    sharedPrerenderData: false,
+    compileTemplate: true,
+    resetAsyncDataToUndefined: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    defaults: {
+      useAsyncData: {
+        deep: true
+      }
+    }
+  },
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false
+    },
+  },
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts',
+    '@nuxt/icon',
+    '@nuxt/image'
+  ],
+  googleFonts: {
+    families: {
+      Montserrat: true,
+    }
+  }
+})
