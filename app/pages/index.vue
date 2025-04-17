f4c7c43c79f8ba97aa42b5ae48b3cf1dc0542d4c
rubatista <inforubenbaptista@gmail.com>
2025-04-17 01:04:03 +0100

    feat: handle with some errors

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -34,7 +34,7 @@ const { data, error } = await useFetch<RecipeResponse>('https://dummyjson.com/re
     <section class="py-20 container">
       <h2 class="text-3xl lg:text-5xl mb-2">Discover, Create, Share</h2>
       <p class="text-lg lg:text-xl mb-8">Check out our most popular recipes!</p>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">
+      <div v-if="!error" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">
         <div
           v-for="recipe in data?.recipes"
           :key="recipe.id"
@@ -73,6 +73,7 @@ const { data, error } = await useFetch<RecipeResponse>('https://dummyjson.com/re
           </div>
         </div>
       </div>
+      <p v-else class="text-xl">Oops, something went wrong. Please try again later!</p>
     </section>
   </main>
 </template>
--- app/pages/recipes/[id].vue
+++ app/pages/recipes/[id].vue
@@ -3,6 +3,13 @@ import { type Recipe } from '../../../types/types'
 const { id } = useRoute().params
 
 const { data, error } = await useFetch<Recipe>(`https://dummyjson.com/recipes/${id}`)
+
+if (error.value) {
+  throw createError({
+    statusCode: error.value?.statusCode,
+    statusMessage: error.value.statusMessage,
+  })
+}
 </script>
 <template>
   <div class="flex flex-col max-w-screen-lg container py-20">
